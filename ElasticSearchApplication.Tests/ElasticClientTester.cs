e1964766c8ce14d8087a080ae3d112b6df5d5e20
Divas Agarwal <dagarwal@tavisca.com>
2017-09-25 10:10:33 +0530

    added solution

---
+++ ElasticSearchApplication.Tests/ElasticClientTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ElasticSearchApplication.Core;
+using Elasticsearch.Net;
+using Nest;
+
+namespace ElasticSearchApplication.Tests
+{
+    [TestClass]
+    public class ElasticClientTester
+    {
+        [TestMethod]
+        public void Get_New_Client_Returns_A_New_Client_When_Called()
+        {
+            var newClient = ElasticSearchClient.GetNewClient();
+            Assert.IsNotNull(newClient);
+            Assert.IsInstanceOfType(newClient, typeof(ElasticClient));
+        }
+    }
+}
